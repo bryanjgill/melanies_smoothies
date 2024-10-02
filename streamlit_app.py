2fda8eacde2b3a50c33c748c5b6499a11e3c854d
bryanjgill <bryanjgill@gmail.com>
2024-10-02 16:27:16 +0000

    Changed app display title.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("Example Streamlit App :cup_with_straw:")
+st.title("My Parents New Healthy Diner")
 st.write(
     """Choose the fruits you want in your custom smoothie!
     """
